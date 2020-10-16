fc331a37352c0398f2bf08a654cf726bb3fc029b
itspanicky <nicky.li92@gmail.com>
2020-10-16 19:28:46 -0400

    feat: initialize firebase app

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCy0mOERnqQOAlDFpDLpkHqpSXkeA9Zowk",
-  authDomain: "chat-app-459fe.firebaseapp.com",
-  databaseURL: "https://chat-app-459fe.firebaseio.com",
-  projectId: "chat-app-459fe",
-  storageBucket: "chat-app-459fe.appspot.com",
-  messagingSenderId: "72441041950",
-  appId: "1:72441041950:web:fde910f86b77e25bc08a0f",
-  measurementId: "G-5KGKH75W8S"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 });
 
 ReactDOM.render(
