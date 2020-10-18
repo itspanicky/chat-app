f2f0ff249155dd451e822566e03f6ff2ae7ed9f0
itspanicky <nicky.li92@gmail.com>
2020-10-17 23:06:28 -0400

    feat: set up dashboard with chat list

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,11 +1,43 @@
 import React from "react";
+import ChatListComponent from "../chatList";
 
 class DashboardComponent extends React.Component {
 
-
-    render() {
-        return (<div>Dashboard Component</div>)
+  constructor() {
+    super();
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
     }
+  }
+
+  render() {
+    const { email, chats, selectedChat} = this.state; 
+    
+    return (
+      <>
+        <div>Dashboard Component</div>
+        <ChatListComponent 
+          history={this.props.history} 
+          newChatBtnFn={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={chats}
+          userEmail={email}
+          selectedChatIndex={selectedChat}
+        />
+      </>
+    )
+  }
+
+  selectChat = () => {
+    console.log("Selected a chat")
+  }
+
+  newChatBtnClicked = () => {
+    console.log("New chat button clicked")
+  }
 };
 
 export default DashboardComponent;
