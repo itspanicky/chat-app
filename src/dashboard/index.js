2b36156d3088aeaa76c560e688b0ac77787d2f29
itspanicky <nicky.li92@gmail.com>
2020-10-17 23:25:05 -0400

    feat: collect user and chats details from firebase on mount

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ChatListComponent from "../chatList";
 
+const firebase = require("firebase");
+
 class DashboardComponent extends React.Component {
 
   constructor() {
@@ -15,7 +17,7 @@ class DashboardComponent extends React.Component {
 
   render() {
     const { email, chats, selectedChat} = this.state; 
-    
+
     return (
       <>
         <div>Dashboard Component</div>
@@ -31,12 +33,31 @@ class DashboardComponent extends React.Component {
     )
   }
 
-  selectChat = () => {
-    console.log("Selected a chat")
+  selectChat = (chatIndex) => {
+    console.log("Selected a chat", chatIndex)
   }
 
-  newChatBtnClicked = () => {
-    console.log("New chat button clicked")
+  newChatBtnClicked = () => this.setState({ newChatFormVisible: true, selectedChat: null })
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async _usr => {
+      if (!_usr) {
+        this.props.history.push("/login")
+      } else {
+        await firebase
+          .firestore()
+          .collection("chats")
+          .where("users", "array-contains", _usr.email)
+          .onSnapshot(async res => {
+            const chats = res.docs.map(_doc => _doc.data());
+            await this.setState({
+              email: _usr.email,
+              chats
+            });
+            console.log(this.state);
+          })
+      }
+    })
   }
 };
 
